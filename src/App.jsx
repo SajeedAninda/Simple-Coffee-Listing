3d01f047fa3afcf715973139f826df94fbe5eac1
SajeedAninda <sajaninda@gmail.com>
2024-08-06 16:33:43 +0600

    Added the two button

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,8 @@ function App() {
       <div className="relative">
         <img src={headerImg} alt="" />
         <div className="h-[100vh] w-full bg-[#111315] relative">
-          <div className="bg-[#1B1D1F] w-[80%] mx-auto px-16 py-16 rounded-xl relative">
-            <img
-              className="absolute top-0 right-72 bottom-0 mx-auto my-auto z-0"
-              src={vectorImg}
-              alt=""
-            />
-            <div className="flex flex-col justify-between items-center gap-4 productsDiv mt-10 z-10 relative">
+          <div className="bg-[#1B1D1F] w-[80%] mx-auto px-16 py-16 rounded-xl ">
+            <div className="flex flex-col justify-between items-center gap-4 productsDiv mt-10 relative">
               <h1 className="text-[32px] font-bold text-white">
                 Our Collection
               </h1>
@@ -23,6 +18,16 @@ function App() {
               <p className="text-[16px] font-semibold text-[#6F757C] text-center w-[60%]">
                 Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.
               </p>
+
+              <div className="flex justify-center items-center gap-8">
+                <button className="bg-transparent hover:bg-[#6F757C] text-white px-3 py-2 rounded-lg font-semibold text-[16px]">
+                  All Products
+                </button>
+
+                <button className="bg-transparent hover:bg-[#6F757C] text-white px-3 py-2 rounded-lg font-semibold text-[16px]">
+                  All Products
+                </button>
+              </div>
             </div>
           </div>
         </div>
